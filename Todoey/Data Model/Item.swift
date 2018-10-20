82ca5a4b8752a4046b323c14be739a84d7069b96
WagonWheel70 <mark@beroldingen.com>
2018-10-19 22:12:34 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mark Stacks on 10/19/18.
+//  Copyright © 2018 Mark Stacks. All rights reserved.
+//
+
+import Foundation
+
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
