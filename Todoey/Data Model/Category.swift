eb89b55b65138e11a7c8eec799ed4877fdcc97c1
WagonWheel70 <mark@beroldingen.com>
2018-10-22 21:22:31 -0700

    Saved Data using Realm

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Mark Stacks on 10/22/18.
+//  Copyright © 2018 Mark Stacks. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
